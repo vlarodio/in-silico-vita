f2752b1e709685aa47185b6dcdc7b40e7176dd49
vlarodio <mrovold@yandex.com>
2026-05-23 07:53:37 +0500

    Initial commit

---
+++ dist/in-silico-vita.cjs
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});function l(e){return`Hello, ${e}!`}exports.hello=l;
+//# sourceMappingURL=in-silico-vita.cjs.map
---
+++ dist/in-silico-vita.js
@@ -0,0 +1,7 @@
+function l(e) {
+  return `Hello, ${e}!`;
+}
+export {
+  l as hello
+};
+//# sourceMappingURL=in-silico-vita.js.map
